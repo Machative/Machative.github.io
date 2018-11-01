5dc75ed2861c86339334718334b53b3f7e3fe429
Aidan Ferry <machjayus@gmail.com>
2018-11-01 17:17:43 -0400

    Create problem1.java

---
+++ res/text/problem1/problem1.java
@@ -0,0 +1,10 @@
+package me.aidan.euler;
+public class Main {
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1;i<1000;i++) {
+			if(i%3==0 || i%5==0) sum+=i;
+		}
+		System.out.println(sum);
+	}
+}
