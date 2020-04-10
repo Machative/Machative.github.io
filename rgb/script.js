d53a7254eef6d2061592b0314f5b2ac34a58eaea
Aidan <machjayus@gmail.com>
2020-04-10 08:24:57 -0400

    awef

--- rgb/script.js
+++ rgb/script.js
@@ -8,3 +8,27 @@ var switchMode = function(mode){
         else cur.style.visibility="hidden";
     }
 }
+var solidInput = document.getElementById("solidinput");
+var onSolidInput = function(){
+    var color = solidInput.value;
+    var submit = document.getElementById("solidsubmit");
+    submit.style.backgroundColor = color;
+}
+var onFadeNumSubmit = function(){
+    var num = document.getElementById("fadenumber").value;
+    var div = document.getElementById("colorsDiv")
+    while(div.firstChild){
+        div.removeChild(div.lastChild);
+    }
+    for(var i=0;i<num;i++){
+        var input = document.createElement("INPUT");
+        input.type="color";
+        div.appendChild(input);
+        input.style.display="inline";
+        input.style.marginRight="5px";
+    }
+}
+/*var fadeSubmit = function(){
+}*/
+/*var solidSubmit = function(){
+}*/
